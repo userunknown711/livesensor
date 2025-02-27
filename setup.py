9e6e87442c6b558d2ee95cace8885e0c4c5634f8
userunknown711 <md.alam.offical@gmail.com>
2025-02-27 16:33:42 +0530

    initial commit

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import find_packages,setup
-#from typing import List
+from typing import List
 
-def get_requirements()->list[str]:
-    requirements_list=list[str]=[]
+def get_requirements()->List[str]:
+    requirements_list:List[str]=[]
     return requirements_list
 
 setup(
     name='sensor',
     version='0.0.1',
-    author="alam",
+    author="userunknown711",
     author_email="md.alam.offical@gmail.com",
     packages=find_packages(),
     install_requires=get_requirements(),#["pymongo"]
